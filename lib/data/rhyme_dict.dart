d6e058fd81d468358d791e5f0831ce2654cda868
Jason Millis <jaymikemill@gmail.com>
2025-09-01 11:07:10 -0600

    refactor: move RhymeSearchProperties from rhyme_search_props.dart to rhyme_dict.dart

--- lib/data/rhyme_dict.dart
+++ lib/data/rhyme_dict.dart
@@ -6,7 +6,6 @@ import 'package:g_rhymes/helpers/log.dart';
 import 'ipa.dart';
 import 'g_dict.dart';
 import 'hive_storage.dart';
-import 'rhyme_search_props.dart';
 
 part 'rhyme_dict.g.dart'; // Generated adapter file
 
@@ -98,4 +97,56 @@ class RhymeDict extends HiveObject {
 
     return rhymes;
   }
+}
+
+/// --- Properties ---
+class RhymeSearchProps {
+  RhymeType rhymeType   = RhymeType.perfect;
+  SpeechType speechType = SpeechType.common;
+  WordType wordType     = WordType.common;
+  int syllables         = 0; // 0 = All
+}
+
+/// --- Enhanced enums with display names ---
+enum RhymeType {
+  all('All'),
+  perfect('Perfect'),
+  near('Near'),
+  vowel('Vowel'),
+  conso('Consonant');
+
+  final String displayName;
+  const RhymeType(this.displayName);
+}
+
+
+const Set<PartOfSpeech> commonSpeechTypes = {
+  PartOfSpeech.noun, PartOfSpeech.verb, PartOfSpeech.adjective,
+  PartOfSpeech.adverb, PartOfSpeech.pronoun
+};
+
+enum SpeechType {
+  all('All', commonSpeechTypes),
+  common('Common', commonSpeechTypes),
+  noun('Nouns', {PartOfSpeech.noun}),
+  verb('Verbs', {PartOfSpeech.verb}),
+  adjective('Adjectives', {PartOfSpeech.adjective}),
+  phrase('Phrases', {PartOfSpeech.phrase}),
+  name('Names', {PartOfSpeech.name}),
+  other('Other', {PartOfSpeech.other});
+
+  final String displayName;
+  final Set<PartOfSpeech> wordPoS;
+  const SpeechType(this.displayName, this.wordPoS);
+}
+
+enum WordType {
+  all('All'),
+  common('Common'),
+  uncommon('Uncommon'),
+  slang('Slang'),
+  vulgar('Vulgar');
+
+  final String displayName;
+  const WordType(this.displayName);
 }
